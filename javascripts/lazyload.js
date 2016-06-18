2cbcb96d26225e7fb15e9f69e536ff0ab7dad5a9
Juan Herrera <david.juanherrera@gmail.com>
2016-06-17 21:33:26 -0500

    CSS deferred

--- javascripts/lazyload.js
+++ javascripts/lazyload.js
@@ -2,10 +2,12 @@ $(function(){
 	if($("body").size()>0){
 		//IE Support
 		if (document.createStyleSheet){
+			document.createStyleSheet('https://fonts.googleapis.com/css?family=Raleway:400,500,600');
 			document.createStyleSheet('build/stylesheets/style.css');
 		}
 		else {   
-			 $("head").append($("<link rel='stylesheet' href='build/stylesheets/style.css' type='text/css' media='screen' />"));
+			$("head").append($("<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,600' rel='stylesheet' type='text/css'>"+
+				"<link rel='stylesheet' href='build/stylesheets/style.css' type='text/css' media='screen' />"));
 		}
 	}
 });
