26e3b079f607a089790c024411da2fbec8fe5972
Juan Herrera <david.juanherrera@gmail.com>
2016-06-18 19:30:33 -0500

    Scroller added

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,25 @@
 $(function(){
-	// "use strict";
-	// console.log("test");
-	// alert("test");
-	// function test(){
-	// 	if (true) {
-			// console.log("test");
 
-	// 	}
-	// }
+	var scrolling=false;
+
+	// jQuery Function to make the actual scrolling
+	function animateScroll(elementClass){
+		console.log(elementClass);
+		$('html,body').animate({scrollTop: $("."+elementClass).offset().top},
+			1000, function(){
+				setTimeout(function(){
+					scrolling=false;
+				}, 50);
+			});
+	}
+
+	// Scroll Buttons assigned function
+	$(".scroller").click(function() {
+		console.log("Entra");
+		if (!scrolling) {
+			scrolling = true;
+			animateScroll($(this).attr("data-number"));
+		}
+	});
+
 });
